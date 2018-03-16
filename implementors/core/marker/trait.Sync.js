3f8296caf1cefca7c00b4fbd1d03769a8940a3b3
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-03-16 17:30:40 +0100

    Semiautomatic update of documentation

---
+++ implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["persistentcache"] = [{text:"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"persistentcache/storage/redis/struct.RedisStorage.html\" title=\"struct persistentcache::storage::redis::RedisStorage\">RedisStorage</a>",synthetic:true,types:["persistentcache::storage::redis::RedisStorage"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"persistentcache/storage/file/struct.FileStorage.html\" title=\"struct persistentcache::storage::file::FileStorage\">FileStorage</a>",synthetic:true,types:["persistentcache::storage::file::FileStorage"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
